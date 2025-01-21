1be8cb2bfb93ba40851223fbf56af28fd82939c2
Cafiero <federico.cafiero@laserromae.it>
2025-01-21 14:56:03 +0100

    fix test

--- __tests__/esercizio2.js
+++ __tests__/esercizio2.js
@@ -1,12 +1,42 @@
 const { describe } = require("node:test");
 
+// function loginFn(email, password) {
+//     if (!email) {
+//         throw new Error("Email is required");
+//     }
+
+//     email = email.trim();
+//     if (!email) {
+//         throw new Error("Email must not have empty space");
+//     }
+
+//     const emailRegex = /\S+@\S+\.\S+/;
+//     if (!emailRegex.test(email)) {
+//         throw new Error("Invalid email format");
+//     }
+
+//     if (email.length > 20) {
+//         throw new Error("Email is too long");
+//     }
+
+//     if (!password) {
+//         throw new Error("Password is required");
+//     }
+
+//     password = password.trim();
+//     if (!password) {
+//         throw new Error("Password must not have empty space");
+//     }
+//     return "Validation passed";
+// }
+
 function loginFn(email, password) {
     if (!email) {
         throw new Error("Email is required");
     }
 
-    email = email.trim();
-    if (!email) {
+    // Controllo spazi iniziali o finali nell'email
+    if (email !== email.trim()) {
         throw new Error("Email must not have empty space");
     }
 
@@ -23,12 +53,14 @@ function loginFn(email, password) {
         throw new Error("Password is required");
     }
 
-    password = password.trim();
-    if (!password) {
+    // Controllo spazi iniziali o finali nella password
+    if (password !== password.trim()) {
         throw new Error("Password must not have empty space");
     }
+
     return "Validation passed";
 }
+
 
 
 describe("AuthTest", () => {
@@ -41,15 +73,15 @@ describe("AuthTest", () => {
     });
 
     test("Should throw error if email has empty spaces", () => {
-        expect(() => loginFn("user3@gmail.com ", "password3")).toThrow("Email cannot have empty space");
+        expect(() => loginFn("user3@gmail.com   ", "password3")).toThrow("Email must not have empty space");
     });
 
     test("Should throw error if email has empty spaces", () => {
-        expect(() => loginFn("user3@gmail.com", "password3 ")).toThrow("Password cannot have empty space");
+        expect(() => loginFn("user3@gmail.com", "password3 ")).toThrow("Password must not have empty space");
     });
 
     test("Should throw error if email is too long", () => {
-        expect(() => loginFn("user3@gmail.comasasasasasasasaasasasasasas.com", "password3")).toThrow("Email is too long");
+        expect(() => loginFn("user3@gmailcomasasasasasasasaasasasasasas.com", "password3")).toThrow("Email is too long");
     });
 
     test("Should throw error if password not provided", () => {
