203eebdee748541fe4b0c0ba8844d2a79e1e592a
Cafiero <federico.cafiero@laserromae.it>
2025-02-06 10:58:59 +0100

    add validations

--- __tests__/registrationForm.js
+++ __tests__/registrationForm.js
@@ -1,4 +1,4 @@
-const { validateName } = require("../registrationForm/formValidation");
+const { validateName, validateEmail, validateParticipants } = require("../registrationForm/formValidation");
 
 describe("Validazione Nome", () => {
     test("Dovrebbe accettare un nome valido", () => {
@@ -6,6 +6,38 @@ describe("Validazione Nome", () => {
     });
 
     test("Dovrebbe rifiutare un nome troppo corto", () => {
-        expect(() => validateName("M")).toThrow("Il nome deve essere lungo almeno 2 caratteri");
+        expect(() => validateName("Ma")).toThrow("Il nome deve essere lungo almeno 3 caratteri");
+    });
+});
+
+describe("Validazione Email", () => {
+    test("Dovrebbe accettare un'email valida", () => {
+        expect(validateEmail("email@mail.com")).toBe(true);
+    });
+
+    test("Dovrebbe rifiutare un'email senza @", () => {
+        expect(() => validateEmail("mail.com")).toThrow("Email non valida");
+    });
+
+    test("Dovrebbe rifiutare un'email senza dominio", () => {
+        expect(() => validateEmail("email@.com")).toThrow("Email non valida");
+    });
+});
+
+describe("Validazione Numero di Partecipanti", () => {
+    test("Dovrebbe accettare un numero valido", () => {
+        expect(validateParticipants(5)).toBe(true);
+    });
+
+    test("Dovrebbe rifiutare un numero inferiore a 1", () => {
+        expect(() => validateParticipants(0)).toThrow("Il numero di partecipanti deve essere tra 1 e 10");
+    });
+
+    test("Dovrebbe rifiutare un numero superiore a 10", () => {
+        expect(() => validateParticipants(11)).toThrow("Il numero di partecipanti deve essere tra 1 e 10");
+    });
+
+    test("Dovrebbe rifiutare un valore non numerico", () => {
+        expect(() => validateParticipants("cinque")).toThrow("Il numero di partecipanti deve essere un numero valido");
     });
 });
---
+++ registrationForm/formValidation.js
@@ -0,0 +1,26 @@
+function validateName(name) {
+    if (typeof name !== "string" || name.length < 3) {
+        throw new Error("Il nome deve essere lungo almeno 3 caratteri");
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        throw new Error("Email non valida");
+    }
+    return true;
+}
+
+function validateParticipants(participants) {
+    if (typeof participants !== "number" || !Number.isInteger(participants)) {
+        throw new Error("Il numero di partecipanti deve essere un numero valido");
+    }
+    if (participants < 1 || participants > 10) {
+        throw new Error("Il numero di partecipanti deve essere tra 1 e 10");
+    }
+    return true;
+}
+
+module.exports = { validateName, validateEmail, validateParticipants };
