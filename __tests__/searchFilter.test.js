aab48ad0af89b19c8808c869cf3c752f014c0f07
Cafiero <federico.cafiero@laserromae.it>
2025-02-11 12:05:02 +0100

    test filters

---
+++ __tests__/searchFilter.test.js
@@ -0,0 +1,21 @@
+import { searchFilter } from '../utils/searchFilter';
+
+describe('searchFilter Utility Function', () => {
+    const sampleData = ['Arancia', 'Mango', 'Pistacchio', 'Mirtillo', 'Ciliegia', 'Cocco'];
+
+    test('Filtra i dati in base al testo inserito', () => {
+        expect(searchFilter(sampleData, 'M')).toEqual(['Mango', 'Mirtillo']);
+    });
+
+    test('Ordina i risultati in ordine alfabetico', () => {
+        expect(searchFilter(sampleData, 'i')).toEqual(['Ciliegia', 'Mirtillo', 'Pistacchio']);
+    });
+
+    test('Limita il numero massimo di risultati', () => {
+        expect(searchFilter(sampleData, 'o', 2)).toEqual(['Cocco', 'Mango']);
+    });
+
+    test('Ritorna un array vuoto se non ci sono corrispondenze', () => {
+        expect(searchFilter(sampleData, 'xyz')).toEqual([]);
+    });
+});
